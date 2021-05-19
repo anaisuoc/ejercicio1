36baf608d7bab96d155e5ac28b7d2fc1b44f4849
anaisuoc <77071549+anaisuoc@users.noreply.github.com>
2021-05-19 19:13:31 +0200

    Update exercises.js

--- src/exercises.js
+++ src/exercises.js
@@ -1,6 +1,22 @@
-export function pp1() {
+export function pp1(conectados) {
+	// Patron de busqueda
+	var patroLi = ".media.media--profile";
+	if (conectados === true) {
+		patroLi = ".media.media--profile.on";
+	} else if (conectados === false) {
+		patroLi = ".media.media--profile.off";
+	}
 
+	var estudiantes = [];
+	// Buscamos los estudiantes con el patron adecuado
+	var liList = document.querySelectorAll(patroLi);
+	for (let item of liList) {
+		const name = document.querySelector(".media.media--profile.user").innerHTML;
+
+	}
 }
+
+
 
 export function pp2() {
 
